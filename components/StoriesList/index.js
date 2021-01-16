59f42d76d90b10fd0af0137841d475363dac3298
Shokry <shokry92@gmail.com>
2021-01-16 22:45:39 +0100

    Update grid style

--- components/StoriesList/index.js
+++ components/StoriesList/index.js
@@ -6,7 +6,9 @@ function StoriesList(props) {
   return (
     <StoryGrid>
       {stories.map((story) => (
-        <Story key={story.slug}>{story.title}</Story>
+        <Story key={story.slug}>
+          <span title={story.title}>{story.title}</span>
+        </Story>
       ))}
     </StoryGrid>
   );
--- components/StoriesList/styles.js
+++ components/StoriesList/styles.js
@@ -2,15 +2,25 @@ import styled from "styled-components";
 
 const StoryGrid = styled.ul`
   list-style: none;
+  margin: 20px;
+  padding: 0;
   display: grid;
   gap: 20px;
-  grid-template-columns: repeat(auto-fill, 220px);
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  justify-content: center;
 `;
 
 const Story = styled.li`
   background-color: palevioletred;
-  height: 350px;
-  color: white;
+  height: 320px;
+  text-align: center;
+  padding-top: 30%;
+
+  > span {
+    color: #491d27;
+    font-size: 2em;
+    text-shadow: 0px 0.5px 0 rgba(255, 255, 255, 0.5);
+  }
 `;
 
 export { StoryGrid, Story };
