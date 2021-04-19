d702ae199b022a6f77cc6ca5c9ecd9b2c627fb7a
Ahmed Shokry <ahmed.shokry@auto1.com>
2021-04-19 18:05:44 +0200

    POC for setting css vars with an inline script

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,33 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+
+function setInitialColorVariables() {
+  function getColorMode() {
+    if (window.localStorage.getItem("prefers-dark")) {
+      return window.localStorage.getItem("prefers-dark") === "true"
+        ? "dark"
+        : "light";
+    }
+
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+
+    return "light";
+  }
+
+  const colorMode = getColorMode();
+
+  if (colorMode === "dark") {
+    document.documentElement.style.setProperty("--bg-color", "#222831");
+    document.documentElement.style.setProperty("--text-color", "#ececec");
+    document.documentElement.style.setProperty("color-scheme", "dark");
+  } else {
+    document.documentElement.style.setProperty("--bg-color", "#d6d5b2");
+    document.documentElement.style.setProperty("--text-color", "initial");
+    document.documentElement.style.setProperty("color-scheme", "light");
+  }
+}
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,5 +54,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(${setInitialColorVariables.toString()})()`,
+            }}
+          ></script>
+        </body>
+      </Html>
+    );
   }
 }
